686f68bcc748fa9895c5622efb65e5b35cb4ac94
Boukri Amine <AMINE_BOUKRI@emsi-edu.ma>
2023-07-21 16:10:54 +0100

    add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chatGPT_with_angular';
+  actions=[
+    {name:'Home',route:'/home',icon:'house'},
+    {name:'GPT',route:'/gpt',icon:'chat'},
+  ]
+  currentAction:any;
+
+  constructor(private router:Router){
+
+  }
+  handleRoute(action: any) {
+    this.currentAction=action;
+
+    this.router.navigateByUrl(action.route);
+  }
 }
